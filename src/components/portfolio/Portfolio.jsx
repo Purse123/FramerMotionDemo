d5ee2558f704f62c2ea37a94f7cf6e10e73c38c8
Purse123 <sahasneupane85@gmail.com>
2024-02-11 22:07:33 +0545

    portfolio finished

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -34,24 +34,25 @@ const Single = ({ items }) => {
 
   const { scrollYProgress } = useScroll({
     target: ref,
+    // offset: ["start start", "end start"],
   });
 
-  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  const y = useTransform(scrollYProgress, [0, 1], [-600, 600]);
 
   return (
-    <section ref={ref}>
-        <div className="container">
-            <div className="wrapper">
-                <div className="imageContainer">
-                    <img src={items.image} alt="" />
-                </div>
-                <motion.div className="textContainer" style={{ y }}>
-                    <h2>{items.title}</h2>
-                    <p>{items.description}</p>
-                    <button>See Demo</button>
-                </motion.div>
-            </div>
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={items.image} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{items.title}</h2>
+            <p>{items.description}</p>
+            <button>See Demo</button>
+          </motion.div>
         </div>
+      </div>
     </section>
   );
 };
